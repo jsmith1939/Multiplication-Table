9ba5532991835a656eaab45a0ac330879aa85b52
jsmith1939 <60932568+jsmith1939@users.noreply.github.com>
2021-10-26 20:17:52 -0400

    intermediate

--- code.js
+++ code.js
@@ -1,27 +1,46 @@
 // Your code here
 // Russel. L helped styling
-document.write("<span class='cell header'> &times; </span>");
 
-let i = 0;
-let row = 0;
-let rowNum = 10;
-let colNum = 10;
+let rowNum = 11;
+let colNum = 11;
 
-while (i <= colNum) {
-	document.write('<span class="cell header">' + i + '</span>');
-	i++;
+console.log(rowNum, colNum);
+
+let button = document.getElementById('submit');
+button.addEventListener('click', newTable);
+
+function newTable() {
+	let row = document.getElementById('row').value;
+	let col = document.getElementById('column').value;
+	if (row === '' || col === '') {
+		return;
+	}
+	row = parseInt(row);
+	col = parseInt(col);
+	console.log(row, col);
+	generateTable(row, col);
 }
-document.write('<br>');
+
 function generateTable(rowNum, colNum) {
+	let i = 0;
+	let row = 0;
+	let table = document.getElementById('multiplication-table');
+	table.innerHTML = "<span class='cell header'> &times; </span>";
+
+	while (i <= colNum) {
+		table.innerHTML += '<span class="cell header">' + i + '</span>';
+		i++;
+	}
+	table.innerHTML += '<br>';
 	while (row <= rowNum) {
 		console.log(rowNum);
-		document.write(`<span class='cell header'> ${row} </span>`);
+		table.innerHTML += `<span class='cell header'> ${row} </span>`;
 		let col = 0;
 		while (col <= colNum) {
-			document.write(`<span class='cell'> ${col * row} </span>`);
+			table.innerHTML += `<span class='cell'> ${col * row} </span>`;
 			col++;
 		}
-		document.write('<br>');
+		table.innerHTML += '<br>';
 		row++;
 	}
 }
